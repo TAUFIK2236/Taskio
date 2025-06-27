5e717aa5cada779ab88c47c524a0e86b70d22823
Taufik <taufikalislam@gmail.com>
2025-06-27 19:25:49 -0400

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Anika Tabasum on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
